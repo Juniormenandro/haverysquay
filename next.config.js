f059e38818225650e92556432c5959fb18f98192
juniorwa <100045666+juniorwa@users.noreply.github.com>
2023-09-19 20:47:45 +0100

    haveysquay

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["upload.wikimedia.org"],
+    domains: ['res.cloudinary.com'],
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/constants/index.ts
@@ -0,0 +1,19 @@
+
+export const bookingDataInitialState = {
+  
+  selectedProductId: "",
+  selectedProductNane: "",
+  selectedProdutPrice: "",
+  step: 0,
+  nome:"",
+  telefone:"",
+  selectedModel:"",
+  selectedColor:"",
+  selectedPayment:"",
+  selectedTime: "",
+  selectedProductDefaultPrice: "",
+  rawPrice: 0,
+  
+};
+
+ 
